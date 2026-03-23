3e6b2f8a58a99d577dd1e80159b4c4cede3ec44b
SamyakH <samkj2002@gmail.com>
2026-03-23 12:58:32 +0530

    USD  TO INR

--- app/admin/packages/page.tsx
+++ app/admin/packages/page.tsx
@@ -170,7 +170,7 @@ export default function Packages() {
 
             <div className="admin-form-row">
               <div className="form-group">
-                <label>Price () *</label>
+                <label>Price (IND) *</label>
                 <input
                   type="number"
                   value={form.price}
